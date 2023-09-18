95ac90d49f9bdacdcb4484751a0dc2f995ad2993
Rijal Kurniawan <rijalkurniawan1994@gmail.com>
2023-09-18 22:17:26 +0700

    baru

--- LATIHAN2_PERSAMAAN_KUADRAT/mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ LATIHAN2_PERSAMAAN_KUADRAT/mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -13,17 +13,18 @@ import java.util.Scanner;
 public class Mavenproject3 {
 
     public static void main(String[] args) {
-        System.out.println("Berikan formula dan pemecahan akar dari persamaan kuadrat x2 + bx + c = 0");
+        System.out.println("Berikan formula dan pemecahan akar dari persamaan kuadrat ax2 + bx + c = 0");
         Scanner masuk = new Scanner(System.in);
-        System.out.print("Masukan nilai b = ");
-        double b = masuk.nextDouble();
-        if (b==0){
+        System.out.print("Masukan nilai a: ");
+        double a = masuk.nextDouble();
+        if (a==0){
             System.out.println("Ini bukan persamaan kuadrat.");
         }else {
-            System.out.print("Masukan nilai c = ");
+            System.out.print("Masukan nilai b: ");
+            double b = masuk.nextDouble();
+            System.out.print("Masukan nilai c: ");
             double c = masuk.nextDouble();
-            
-            double D = Math.pow(b, 2) - (4 * b * c);
+            double D = Math.pow(b, 2) - (4 * a * c);
             if (D < 0) {
                 System.out.println("Akarnya tidak nyata.");
             } else if (D > 0) {
@@ -31,8 +32,8 @@ public class Mavenproject3 {
             } else if (D == 0) {
                 System.out.println("Akarnya nyata dan setara.");
             }
-            double eq1 = (-b) / (2 * b);
-            double eq2 = (Math.sqrt(D)) / (2 * b);
+            double eq1 = (-b) / (2 * a);
+            double eq2 = (Math.sqrt(D)) / (2 * a);
             double x1 = eq1 - eq2;
             double x2 = eq1 + eq2;
             System.out.println("Nilai X nya adalah " + x1 + "," + x2 + ".");
