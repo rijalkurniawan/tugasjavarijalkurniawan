0a8e9becf924e18bfc6e37175e46b1ee9a0b1bcb
Rijal Kurniawan <rijalkurniawan1994@gmail.com>
2023-09-18 13:47:32 +0700

    tugas java latihan2

---
+++ LATIHAN2_PERSAMAAN_KUADRAT/mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Berikan formula dan pemecahan akar dari persamaan kuadrat x2 + bx + c = 0");
+        Scanner masuk = new Scanner(System.in);
+        System.out.print("Masukan nilai b = ");
+        double b = masuk.nextDouble();
+        if (b==0){
+            System.out.println("Ini bukan persamaan kuadrat.");
+        }else {
+            System.out.print("Masukan nilai c = ");
+            double c = masuk.nextDouble();
+            
+            double D = Math.pow(b, 2) - (4 * b * c);
+            if (D < 0) {
+                System.out.println("Akarnya tidak nyata.");
+            } else if (D > 0) {
+                System.out.println("Akarnya nyata dan berbeda.");
+            } else if (D == 0) {
+                System.out.println("Akarnya nyata dan setara.");
+            }
+            double eq1 = (-b) / (2 * b);
+            double eq2 = (Math.sqrt(D)) / (2 * b);
+            double x1 = eq1 - eq2;
+            double x2 = eq1 + eq2;
+            System.out.println("Nilai X nya adalah " + x1 + "," + x2 + ".");
+        }
+
+    }
+}
